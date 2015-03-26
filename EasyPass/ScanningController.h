2846a07eea8363e68fad6372518e05884017ab3e
Evgeny Demidchenko <promocoding@gmail.com>
2015-03-26 16:59:51 +0200

    Add Scanning Controller

---
+++ EasyPass/ScanningController.h
@@ -0,0 +1,5 @@
+#import <UIKit/UIKit.h>
+
+@interface ScanningController : UITabBarController <UITabBarControllerDelegate>
+
+@end
---
+++ EasyPass/ScanningController.m
@@ -0,0 +1,37 @@
+#import "ScanningController.h"
+
+@interface ScanningController ()
+
+@end
+
+@implementation ScanningController
+
+- (id)initWithCoder:(NSCoder *)aDecoder {
+    if (self = [super initWithCoder: aDecoder])  {
+        self.tabBarController.delegate = self;
+        self.delegate = self;
+    }
+    
+    return  self;
+}
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+
+    if ([self.viewControllers count] > 0) {
+        self.navigationItem.title = [self.viewControllers.firstObject title];
+    }
+}
+
+- (void)tabBarController:(UITabBarController *)tabBarController didSelectViewController:(UIViewController *)viewController {
+    UIViewController *destination = viewController;
+    
+    if ([viewController isKindOfClass: [UINavigationController class]]) {
+        UINavigationController *controller = (UINavigationController *)viewController;
+        destination = controller.visibleViewController;
+    }
+    
+    self.tabBarController.navigationItem.title = destination.title;
+}
+
+@end
