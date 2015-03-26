1138ab19c067599ea9d9bf55012d0b19034f448d
Evgeny Demidchenko <promocoding@gmail.com>
2015-03-26 23:21:50 +0200

    Add demo chart

--- EasyPass/MasterViewController.h
+++ EasyPass/MasterViewController.h
@@ -1,5 +1,8 @@
 #import <UIKit/UIKit.h>
+#import "PNChart.h"
 
 @interface MasterViewController : UIViewController
+
+@property (nonatomic) PNCircleChart *availableParkingLots;
 
 @end
--- EasyPass/MasterViewController.m
+++ EasyPass/MasterViewController.m
@@ -21,8 +21,30 @@
     return  self;
 }
 
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    [self availableParkingLotsChart];
+}
+
 - (void)scanCode {
     [self performSegueWithIdentifier: @"Scanning" sender:nil];
+}
+
+- (void)availableParkingLotsChart {
+    CGRect rect = CGRectMake(0,150.0, SCREEN_WIDTH, 100.0);
+    self.availableParkingLots = [[PNCircleChart alloc]
+                                 initWithFrame: rect
+                                 total: [NSNumber numberWithInt: 100]
+                                 current: [NSNumber numberWithInt: 60]
+                                 clockwise: YES
+                                 shadow: YES
+                                 shadowColor: PNGrey];
+    
+    self.availableParkingLots.backgroundColor = [UIColor clearColor];
+    [self.availableParkingLots setStrokeColor: PNGreen];
+    [self.availableParkingLots strokeChart];
+    
+    [self.view addSubview: self.availableParkingLots];
 }
 
 @end
